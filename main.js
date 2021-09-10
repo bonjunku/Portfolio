7644aeddab06ef2d92d707c8c2c6a1cff931a6a5
J007_구본준 <ku96kbj@naver.com>
2021-09-10 14:24:38 +0900

    JS:Navbar

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-//Make navbar transparent when it is on the top
+// Make navbar transparent when it is on the top
 document.addEventListener('scroll',()=>{
   if(window.scrollY>navbarHeight){
     navbar.classList.add('navbar--dark');
@@ -11,4 +11,18 @@ document.addEventListener('scroll',()=>{
   else{
     navbar.classList.remove('navbar--dark');
   }
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+  const target= event.target;
+  const link = target.dataset.link;
+  if (link == null){
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior:'smooth'});  
 });
